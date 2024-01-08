3416c8c838de381a26caf04149455ede2ead5afe
Domagoj Kristov <domagojkk@gmail.com>
2024-01-08 18:29:01 +0100

    Added Domain project, DbContext, Audit, Chore entity

---
+++ AuthDemo.Domain/Audit/IAuditableEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace AuthDemo.Domain.Audit
+{
+    public interface IAuditableEntity
+    {
+        public long? CreatedBy { get; set; }
+        public long? UpdatedBy { get; set; }
+        DateTime? CreatedAt { get; set; }
+        DateTime? UpdatedAt { get; set; }
+
+    }
+}
--- AuthDemo.Web/Program.cs
+++ AuthDemo.Web/Program.cs
@@ -1,7 +1,8 @@
+using AuthDemo.Domain;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+// builder.Services.RegisterDomainServices(builder.Configuration);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
